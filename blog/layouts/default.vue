b4cea451f73fb8947513362a30d9111d0976f141
luohuidong <luohuidong@126.com>
2023-01-16 00:06:32 +0800

    add post page

---
+++ blog/layouts/default.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const navs = [{ to: "/", text: "home" }];
+</script>
+
+<template>
+  <div class="mx-auto flex min-h-screen w-3/4 flex-col">
+    <header class="mx-auto flex h-24 w-3/4 items-center justify-between gap-3">
+      <h1 class="text-center text-3xl font-bold">Life of coding</h1>
+
+      <div>
+        <NuxtLink
+          v-for="item of navs"
+          :to="item.to"
+          class="text-neutral-400 underline"
+        >
+          {{ item.text }}
+        </NuxtLink>
+      </div>
+    </header>
+
+    <div class="flex-1">
+      <slot></slot>
+    </div>
+
+    <footer class="mt-10 h-10 text-center">
+      <a
+        class="text-sm text-neutral-400 underline"
+        href="https://beian.miit.gov.cn/"
+        >粤ICP备18108480号-1</a
+      >
+    </footer>
+  </div>
+</template>
---
+++ blog/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { list } = await $fetch("/api/posts");
+</script>
+
+<template>
+  <div class="mx-auto w-1/2 pt-4">
+    <ul class="mt-10">
+      <li v-for="item in list" :key="item.date" class="mb-2">
+        <NuxtLink :to="`/post/${item.filename}`">
+          <div class="flex flex-col rounded p-4 hover:bg-slate-50">
+            <h2 class="mb-4 text-xl font-bold underline">{{ item.title }}</h2>
+            <time :date="item.date" class="mb-2 text-sm text-gray-500">
+              {{ item.date }}
+            </time>
+          </div>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
