c7d49ff93fde47b4f24cec3e35be3a0a5751f480
luohuidong <luohuidong01@126.com>
2023-01-24 22:00:35 +0800

    change blog server api base

--- packages/blog/nuxt.config.ts
+++ packages/blog/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      blogServer: process.env.NUXT_BLOG_SERVER || "http://localhost:3000",
+      blogServer: process.env.NUXT_BLOG_SERVER || "http://localhost:8000",
     },
   },
   css: ["~/assets/css/main.css"],
