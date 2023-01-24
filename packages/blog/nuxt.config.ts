e53491c611365fd16e03773906487fcca1379f4e
luohuidong <luohuidong01@126.com>
2023-01-24 22:59:56 +0800

    change blog server fallback api base

--- packages/blog/nuxt.config.ts
+++ packages/blog/nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      blogServer: process.env.NUXT_BLOG_SERVER || "http://localhost:8000",
+      blogServer:
+        process.env.NUXT_BLOG_SERVER || "https://api.luohuidong.cn/blog",
     },
   },
   css: ["~/assets/css/main.css"],
