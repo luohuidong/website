194e736c962dc71a731590b94c5980f1d3527d32
luohuidong <luohuidong01@126.com>
2023-01-25 18:16:51 +0800

    add github link

--- packages/blog/layouts/default.vue
+++ packages/blog/layouts/default.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-const navs = [{ to: "/", text: "home" }];
+const navs = [
+  { to: "/", text: "首页" },
+  { to: "https://github.com/luohuidong", text: "GitHub" },
+];
 </script>
 
 <template>
   <div class="mx-auto flex min-h-screen w-3/4 flex-col">
-    <header class="mx-auto flex h-24 w-3/4 items-center justify-between gap-3">
-      <h1 class="text-center text-3xl font-bold">Life of coding</h1>
-
-      <div>
-        <NuxtLink
-          v-for="item of navs"
-          :to="item.to"
-          class="text-neutral-400 underline"
-        >
-          {{ item.text }}
-        </NuxtLink>
-      </div>
+    <header class="mx-auto flex h-24 w-3/4 items-center justify-end gap-3">
+      <NuxtLink
+        v-for="item of navs"
+        :to="item.to"
+        class="text-neutral-400 underline"
+      >
+        {{ item.text }}
+      </NuxtLink>
     </header>
 
     <div class="flex-1">
@@ -26,8 +25,9 @@ const navs = [{ to: "/", text: "home" }];
       <a
         class="text-sm text-neutral-400 underline"
         href="https://beian.miit.gov.cn/"
-        >粤ICP备18108480号-1</a
       >
+        粤ICP备18108480号-1
+      </a>
     </footer>
   </div>
 </template>
