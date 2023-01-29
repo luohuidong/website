fe12a1812cc1c1b40e6db436514ad67da6c69c40
luohuidong <luohuidong01@126.com>
2023-01-29 22:41:18 +0800

    change blog path

--- project-build.sh
+++ project-build.sh
@@ -2,7 +2,7 @@ ROOT_DIR=$(pwd)
 
 for PROJECT in $(ls packages) 
 do
-  cd ${ROOT_DIR}/packages/${PROJECT}/
+  cd ${ROOT_DIR}/blog/${PROJECT}/
   pnpm build 
   cd ${ROOT_DIR}
 done
--- project-install-deps.sh
+++ project-install-deps.sh
@@ -2,7 +2,7 @@ ROOT_DIR=$(pwd)
 
 for PROJECT in $(ls packages) 
 do
-  cd ${ROOT_DIR}/packages/${PROJECT}/
+  cd ${ROOT_DIR}/blog/${PROJECT}/
   pnpm install 
   cd ${ROOT_DIR}
 done
