6c393a1fea07f1f72d5974c56dbee0b9f8012bb0
luohuidong <luohuidong01@126.com>
2023-01-31 18:06:01 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "3000-blog",
       exec_mode: "cluster",
       instances: "max",
-      script: "./packages/blog/.output/server/index.mjs",
+      script: "./website/blog/.output/server/index.mjs",
       env: {
         NUXT_BLOG_SERVER: "https://api.luohuidong.cn/blog",
       },
@@ -13,7 +13,7 @@ module.exports = {
       name: "8000-blog-server",
       exec_mode: "cluster",
       instances: "max",
-      script: "./packages/server/dist/index.js",
+      script: "./website/server/dist/index.js",
     },
   ],
 };
