8695ff6a40554bd49946683cb99393fc3350bfac
luohuidong <luohuidong01@126.com>
2023-01-31 21:08:43 +0800

    chagne note list style

--- website/blog/pages/notes/index.vue
+++ website/blog/pages/notes/index.vue
@@ -17,7 +17,7 @@ const result = await $fetch<{
       <li
         v-for="item in result.list"
         :key="item.noteDirName"
-        class="flex h-10 items-center border-y px-5 text-neutral-500 hover:bg-neutral-100 hover:text-neutral-600"
+        class="flex h-10 items-center border-t px-5 text-neutral-500 last:border-b hover:bg-neutral-100 hover:text-neutral-600"
       >
         <NuxtLink :to="`/notes/${item.noteDirName}`" class="w-full">
           {{ item.noteTitle }}
