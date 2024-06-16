cae2ed47267e482a145e29924fd4139e1bd0bcbd
luohuidong <luohuidong01@126.com>
2024-06-16 18:29:51 +0800

    add TOC

---
+++ components/ArticleTOC.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { TocLink } from "@nuxt/content/types";
+
+const props = defineProps<{
+  data?: TocLink[];
+}>();
+console.log("data", props.data);
+</script>
+
+<template>
+  <ul class="pl-3">
+    <li v-for="item in data" :key="item.id">
+      <a
+        :href="`#${item.id}`"
+        class="block truncate text-sm leading-7 text-neutral-500 transition-colors hover:text-neutral-700"
+      >
+        {{ item.text }}
+      </a>
+
+      <ArticleTOC :data="item.children"></ArticleTOC>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
--- pages/blog/[name].vue
+++ pages/blog/[name].vue
@@ -1,9 +1,39 @@
 <template>
-  <div class="mx-auto max-w-3xl">
-    <ContentDoc v-slot="{ doc }">
-      <Article :title="doc.title" :date="formatDate(doc.date)">
-        <ContentRenderer :value="doc" />
-      </Article>
+  <div class="w-full">
+    <ContentDoc>
+      <template #="{ doc }">
+        <div class="flex">
+          <div class="hidden 2xl:block 2xl:w-72"></div>
+
+          <div class="flex-1">
+            <Article
+              :title="doc.title"
+              :date="formatDate(doc.date)"
+              :toc="doc.body?.toc"
+            >
+              <ContentRenderer :value="doc" />
+            </Article>
+          </div>
+
+          <div class="hidden lg:block lg:w-72 lg:pt-10">
+            <ArticleTOC
+              class="sticky top-10"
+              :data="doc.body?.toc?.links"
+            ></ArticleTOC>
+          </div>
+        </div>
+      </template>
+
+      <template #not-found>
+        <div class="mt-20 text-center">
+          <h1 class="text-4xl font-bold">404</h1>
+          <p class="mt-4 text-gray-600">Not Found</p>
+        </div>
+      </template>
+
+      <template #empty>
+        <h1>Document is empty</h1>
+      </template>
     </ContentDoc>
   </div>
 </template>
